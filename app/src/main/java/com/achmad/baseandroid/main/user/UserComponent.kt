586877503dfad2df1f6e3425ed53090c3544c93f
achmadfachrudin <a2fachrudin@gmail.com>
2022-08-09 22:52:37 +0700

    chore(github): cleanup code

--- app/src/main/java/com/achmad/baseandroid/main/user/UserComponent.kt
+++ app/src/main/java/com/achmad/baseandroid/main/user/UserComponent.kt
@@ -11,7 +11,6 @@ import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
